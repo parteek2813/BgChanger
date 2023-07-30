cde9799c17dd64ac78631d1909da5ff9a46a84f8
parteek kumar <digitalparteek@gmail.com>
2023-07-30 17:56:47 +0530

    #25 Toggle, Show and Hide Components

--- App3.tsx
+++ App3.tsx
@@ -4,22 +4,29 @@ import React, {useEffect, useState} from 'react';
 export default function App3() {
   const [count, setCount] = useState(0);
   const [data, setData] = useState(100);
+  const [toggle, setToggle] = useState(false);
+
+  useEffect(() => {}, [toggle]);
 
   useEffect(() => {
     console.log('Hello');
   }, [count, data]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log('This will run once during mounting phase');
+  }, []);
 
   return (
     <View>
       <Text>App3</Text>
       <Button title="updateCount" onPress={() => setCount(count + 1)} />
       <Button title="updateData" onPress={() => setData(data + 1)} />
+      <Button title="toggle" onPress={() => setToggle(!toggle)} />
 
       <Text>{count}</Text>
       <Text>{data}</Text>
       <User data={data} count={count} />
+      <Teacher toggle={toggle} />
     </View>
   );
 }
@@ -44,6 +51,24 @@ const User: React.FC<UserProps> = props => {
   return (
     <View>
       <Text>User Component</Text>
+    </View>
+  );
+};
+
+interface TeacherProps {
+  toggle: boolean;
+}
+const Teacher: React.FC<TeacherProps> = props => {
+  const {toggle} = props;
+
+  return (
+    <View>
+      {toggle && (
+        <>
+          <Text>Hello i am in teacher component</Text>
+          <Text> Is it working</Text>
+        </>
+      )}
     </View>
   );
 };
